3f1dccd748f93e4e8c4ea9190c1ff6ce9068a333
yanyiwu <wuyanyi09@foxmail.com>
2015-06-07 11:17:26 +0800

    update demo && add dict/

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 all: 
-	g++ -o demo -O3 -DLOGGER_LEVEL=LL_WARN -Wall -g -I./include demo.cpp 
+	g++ -o demo -O3 -DLOGGER_LEVEL=LL_WARN -Wall -g demo.cpp 
 clean:
 	rm -f *.o *.a demo
--- demo.cpp
+++ demo.cpp
@@ -1,26 +1,48 @@
-#include <iostream>
-#include <fstream>
-#include <cstdlib>
-#include <cstdio>
-#include "MixSegment.hpp"
-//#include "MPSegment.hpp"
-//#include "HMMSegment.hpp"
-//#include "FullSegment.hpp"
-//#include "QuerySegment.hpp"
+#include "include/Application.hpp"
 
 using namespace CppJieba;
 
-const char * const dict_path =  "dict/jieba.dict.utf8";
-const char * const model_path = "dict/hmm_model.utf8";
+int main(int argc, char** argv) {
+  CppJieba::Application app("./dict/jieba.dict.utf8",
+                            "./dict/hmm_model.utf8",
+                            "./dict/user.dict.utf8",
+                            "./dict/idf.utf8",
+                            "./dict/stop_words.utf8");
+  vector<string> words;
+  string result;
+  string s = "我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。";
 
-const char * const test_lines = "我来到南京市长江大桥";
+  cout << "\n\e[32m" << "[demo] METHOD_MP" << "\e[0m\n"; // colorful
+  app.cut(s, words, METHOD_MP);
+  cout << join(words.begin(), words.end(), "/") << endl;
 
-int main(int argc, char ** argv)
-{
-    /*segment init may take a few seconds.*/
-    MixSegment segment(dict_path, model_path);
-    vector<string> words;
-    segment.cut(test_lines, words);
-    cout << words << endl;
-    return 0;
+  cout << "\n\e[32m" << "[demo] METHOD_HMM" << "\e[0m\n"; // colorful
+  app.cut(s, words, METHOD_HMM);
+  cout << join(words.begin(), words.end(), "/") << endl;
+
+  cout << "\n\e[32m" << "[demo] METHOD_MIX" << "\e[0m\n"; // colorful
+  app.cut(s, words, METHOD_MIX);
+  cout << join(words.begin(), words.end(), "/") << endl;
+
+  cout << "\n\e[32m" << "[demo] METHOD_FULL" << "\e[0m\n"; // colorful
+    app.cut(s, words, METHOD_FULL);
+    cout << join(words.begin(), words.end(), "/") << endl;
+
+  cout << "\n\e[32m" << "[demo] METHOD_QUERY" << "\e[0m\n"; // colorful
+    app.cut(s, words, METHOD_QUERY);
+    cout << join(words.begin(), words.end(), "/") << endl;
+
+  cout << "\n\e[32m" << "[demo] TAGGING" << "\e[0m\n"; // colorful
+  vector<pair<string, string> > tagres;
+  app.tag(s, tagres);
+  cout << s << endl;
+  cout << tagres << endl;;
+
+  cout << "\n\e[32m" << "[demo] KEYWORD" << "\e[0m\n"; // colorful
+  vector<pair<string, double> > keywordres;
+  app.extract(s, keywordres, 5);
+  cout << s << endl;
+  cout << keywordres << endl;
+
+  return EXIT_SUCCESS;
 }
